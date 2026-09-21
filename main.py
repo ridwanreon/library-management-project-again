ba961186d941655efb31b6f0136a4f687049a376
ridwanreon <reonrahman8@gmail.com>
2026-09-21 23:24:23 +0600

    update

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ origins = [
     "http://127.0.0.1:5174",
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    "https://animated-mousse-2b4a3a.netlify.app"
+    "https://luxury-paprenjak-5e58d3.netlify.app/"
 
 ]
 
